1c35d3793a228273b2de4a5d814ca25f65e363d8
ismailaissami <aissami.ismail@gmail.com>
2021-09-15 19:21:48 +0200

    pawn move debug

--- src/com/ynov/hal/pieces/Pawn.java
+++ src/com/ynov/hal/pieces/Pawn.java
@@ -24,22 +24,30 @@ public class Pawn extends Piece {
 	@Override
 	public boolean checkMove( int originX, int originY, int targetX, int targetY, boolean toKill) {
 		int x = Math.abs(originX - targetX);
-        int y = Math.abs(originY - targetY);
+        int y = originY - targetY;
         int specialCase = Math.abs(originY - targetY);
         
-        // Cas spéciale, puisque au début de la partie, nous avons la possibilité d'avancer avec 2 cases ou 1 seule
-        // selon le choix du joueur. 
-        System.out.println("[CALC] x="+x+", y="+y+" - Pawn");
-        if(specialCase==2  && (originY==1||originY==6)) {
-        	return true;
+        if(toKill) {
+        	if(Math.abs(y)==1 && x==1) {
+            	return true;
+            }
         }
-        
-        // avancement d'une seule case (CAS NORMALE)
-        else if(y==1 && x==0) {
-        	return true;
+        else {
+        	// Cas spéciale, puisque au début de la partie, nous avons la possibilité d'avancer avec 2 cases ou 1 seule
+            // selon le choix du joueur. 
+            System.out.println("[CALC] x="+x+", y="+y+" - Pawn");
+            if(specialCase==2  && (originY==1||originY==6)) {
+            	return true;
+            }
+            // avancement d'une seule case (CAS NORMALE), pas de retour en arrière.
+            else if(y==-1 && x==0 && this.getColor()==ChessColor.WHITE) {
+            	return true;
+            }
+            else if(y==1 && x==0 && this.getColor()==ChessColor.BLACK) {
+            	return true;
+            }
         }
-        
-        return false;
+    	return false;
 	}
 
 }
